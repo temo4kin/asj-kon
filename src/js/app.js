bfab5171f57be693018f44483d2f41aeca1aff27
Temo4kin <temo4kinn@gmail.com>
2025-06-09 10:06:19 +0300

    Add new class and methods for it

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,25 @@ class Team {
     constructor() {
         this.members = new Set();
     }
+    add(character) {
+        if (this.members.has(character)) {
+            throw new Error('Такой персонаж уже есть в команде');
+        }
+        this.members.add(character);
+    }
+    addAll(...characters) {
+        characters.forEach(character => this.members.add(character));
+    }
+    toArray() {
+        return Array.from(this.members);
+    }
 }
+
+const team = new Team();
+team.add('Лучник');
+team.add('Маг');
+team.add('Воин');
+team.add('Маг');
+team.addAll('Мечник', 'Лучник');
+team.addAll('Король', 'Королева');
+console.log(team.toArray());
